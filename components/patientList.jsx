ad2c5d4aee05d4e20f6924b2ae92d47d1b956a9e
Louis Hugens <louishugens@gmail.com>
2023-08-23 14:01:08 -0400

    change ui of patients list page

--- components/patientList.jsx
+++ components/patientList.jsx
@@ -18,19 +18,18 @@ export default function PatientList({patients, doctorId}) {
       <div className="grid gap-4 grid-cols-3 mt-4">
         {
           patients.map((patient) => (
-            <div className="basis-1/3 h-auto shadow-md rounded-lg p-4" key={patient.id}>
-              <p className="text-lg  font-bold text-slate-900">{patient.firstname} {patient.lastname}</p>
-              <p className="text-sm font-light text-slate-900"><span className="font-medium"> 
+            <div className="basis-1/3 h-auto  rounded-lg p-4 border border-slate-300 hover:border-green-400 hover:shadow-md" key={patient.id}>
+              <p className="text-base  font-semibold text-slate-900">{patient.firstname} {patient.lastname}</p>
+              <p className="text-sm font-light text-slate-900 mt-2 mb-4"><span className="font-medium"> 
                 {formatDistanceToNow(new Date(patient.birthdate))}</span> hold</p>
-              <div className="flex flex-row justify-between mt-4">
-                <Link href={`/user/patients/${patient.id}`} className="py-2 px-4 rounded-full bg-green-500 text-sm font-medium" >
+              <div className="flex flex-row justify-between mt-6">
+                <Link href={`/user/patients/${patient.id}`} className="py-2 px-4 rounded-full bg-muted text-primary text-xs font-medium" >
                   View
                 </Link>
-                <Link href={`/user/patients/${patient.id}/add-appointment`} className="py-2 px-4 rounded-full bg-green-500 text-sm text-white font-medium " >
+                <Link href={`/user/patients/${patient.id}/add-appointment`} className="py-2 px-4 rounded-full bg-primary text-xs text-white font-medium " >
                   Add Appointment
                 </Link>
               </div>
-
             </div>
           ))
         }
