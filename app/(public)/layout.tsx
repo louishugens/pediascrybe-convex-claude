011a6115ee2a38b27bbad3bcaead0294f8f8c4e5
Louis Hugens <louishugens@gmail.com>
2023-09-21 09:17:34 -0400

    add metadata to public pages

---
+++ app/(public)/layout.tsx
@@ -0,0 +1,18 @@
+import { SiteHeader } from '@/components/siteHeader';
+import Footer from '@/components/Footer';
+import type { Metadata } from 'next'
+ 
+export const metadata: Metadata = {
+  title: 'Pediatric Care, Elevated by AI Integration | Pediascrybe',
+  description: 'Tailored for dedicated pediatricians, Pediascrybe streamlines patient data management like never before. Harness the power of advanced AI integration to elevate your practice. Experience efficiency and precision like never before.',
+}
+
+export default async function RootLayout({children}) {
+  return (
+    <div className='relative flex min-h-screen flex-col'>
+      <SiteHeader />
+      <div className='flex-1'>{children}</div>
+      <Footer />
+    </div>
+  )
+}
