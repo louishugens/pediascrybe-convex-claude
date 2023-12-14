4c924aa75e28739be8cba7144c2ce6f6fcd6dd3f
Louis Hugens <louishugens@gmail.com>
2023-12-14 15:40:14 -0500

    add posthog

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,8 @@ import { SiteHeader } from '@/components/siteHeader';
 import Footer from '@/components/Footer';
 import type { Metadata } from 'next'
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import {PHProvider, PostHogPageview} from './provider';
+import { Suspense } from 'react';
  
 export const metadata: Metadata = {
   title: 'Pediatric Care, Elevated by AI Integration | Pediascrybe',
@@ -27,7 +29,7 @@ const montserrat = Montserrat({
   subsets: ['latin']
 })
 
-export const dynamic = 'force-dynamic'
+// export const dynamic = 'force-dynamic'
 export default async function RootLayout({children}) {
   // const supabase = createServerComponentClient({cookies})
 
@@ -37,12 +39,17 @@ export default async function RootLayout({children}) {
 
   return (
     <html lang="en" className={montserrat.className}>
-      <body className='min-h-screen bg-background font-sans antialiased' >  
-        <GA />  
-        <div className='flex-1'>{children}</div>
-        <AnalyticsWrapper />
-        <SpeedInsights />
-      </body>
+      {/* <Suspense>
+        <PostHogPageview />
+      </Suspense> */}
+      <PHProvider>
+        <body className='min-h-screen bg-background font-sans antialiased' >  
+          <GA />  
+          <div className='flex-1'>{children}</div>
+          <AnalyticsWrapper />
+          <SpeedInsights />
+        </body>
+      </PHProvider>
     </html>
   )
 }
